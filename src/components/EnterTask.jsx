edfa18abe3fc72af7d57ca8a6986b4b4c14e2c95
Keerthivasan00 <keerthivasan63850@gmail.com>
2025-10-31 23:34:37 +0530

    connect to Backend to fetch date from db

--- src/components/EnterTask.jsx
+++ src/components/EnterTask.jsx
@@ -1,38 +1,67 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function EnterTask({ onClose }) {
+  const [title, setTitle] = useState("");
+  const [duedate, setDueDate] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newTask = {
+      description,
+      duedate,
+      title,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/task/save",
+        newTask
+      );
+      console.log("Task Added:", response.data);
+      alert("Task saved successfully!");
+    } catch (error) {
+      console.error("Error saving task:", error);
+    }
+  };
+
   return (
     <div className="fixed top-50 left-2/5 w-96 bg-purple-200 bg-opacity-40 flex items-center justify-center">
       <div className="bg-white p-6 rounded-xl w-96 shadow-lg">
-        <h2 className="text-xl font-bold mb-4">Title</h2>
-        <input
-          type="text"
-          placeholder="Enter task name"
-          className="border p-2 w-full rounded mb-4"
-        />
-        <h2 className="text-xl font-bold mb-4">Description</h2>
-        <input
-          type="text"
-          placeholder="Enter Description"
-          className="border p-2 w-full rounded mb-4"
-        />
-        <h2 className="text-xl font-bold mb-4">Date</h2>
-        <input
-          type="date"
-          placeholder="Enter task name"
-          className="border p-2 w-full rounded mb-4"
-        />
-        <div className="flex justify-end gap-3">
-          <button
-            onClick={onClose}
-            className="bg-gray-400 px-4 py-2 rounded text-white"
-          >
-            Cancel
-          </button>
-          <button className="bg-purple-500 px-4 py-2 rounded text-white">
-            Add
-          </button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-xl font-bold mb-4">Title</h2>
+          <input
+            type="text"
+            placeholder="Enter task name"
+            className="border p-2 w-full rounded mb-4"
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <h2 className="text-xl font-bold mb-4">Description</h2>
+          <input
+            type="text"
+            placeholder="Enter Description"
+            className="border p-2 w-full rounded mb-4"
+            onChange={(event) => setDescription(event.target.value)}
+          />
+          <h2 className="text-xl font-bold mb-4">Date</h2>
+          <input
+            type="date"
+            placeholder="Enter task name"
+            className="border p-2 w-full rounded mb-4"
+            onChange={(event) => setDueDate(event.target.value)}
+          />
+          <div className="flex justify-end gap-3">
+            <button
+              onClick={onClose}
+              className="bg-gray-400 px-4 py-2 rounded text-white"
+            >
+              Cancel
+            </button>
+            <button type="submit" className="bg-purple-500 px-4 py-2 rounded text-white">
+              Add
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,78 +1,96 @@
 import del from "../assets/delete.png";
 import edit from "../assets/edit.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
 
 function TaskCard() {
-  const tasks = [
-    {
-      id: 1,
-      title: "java",
-      desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-      date: "11/12/2025",
-    },
-    {
-      id: 2,
-      title: "css",
-      desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-      date: "11/12/2025",
-    },
-    {
-      id: 3,
-      title: "js",
-      desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-      date: "11/12/2025",
-    },
-    {
-      id: 4,
-      title: "mysql",
-      desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-      date: "11/12/2025",
-    },
-    {
-      id: 5,
-      title: "spring",
-      desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-      date: "11/12/2025",
-    },
-    {
-      id: 6,
-      title: "tailwind",
-      desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-      date: "11/12/2025",
-    },
-    {
-      id: 7,
-      title: "bootstrap",
-      desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-      date: "11/12/2025",
-    },
-    {
-        id:8,
-        title:"swing",
-        desp:"dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-        date:"11/12/2025"
-    },
-    {
-        id:9,
-        title:"servlet",
-        desp:"dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-        date:"11/12/2025"
-    },
-    {
-        id:10,
-        title:"hibernate",
-        desp:"dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
-        date:"11/12/2025"
-    }
-  ];
+  // const tasks = [
+  //   {
+  //     id: 1,
+  //     title: "java",
+  //     desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //     date: "11/12/2025",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "css",
+  //     desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //     date: "11/12/2025",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "js",
+  //     desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //     date: "11/12/2025",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "mysql",
+  //     desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //     date: "11/12/2025",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "spring",
+  //     desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //     date: "11/12/2025",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "tailwind",
+  //     desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //     date: "11/12/2025",
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "bootstrap",
+  //     desp: "dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //     date: "11/12/2025",
+  //   },
+  //   {
+  //       id:8,
+  //       title:"swing",
+  //       desp:"dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //       date:"11/12/2025"
+  //   },
+  //   {
+  //       id:9,
+  //       title:"servlet",
+  //       desp:"dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //       date:"11/12/2025"
+  //   },
+  //   {
+  //       id:10,
+  //       title:"hibernate",
+  //       desp:"dbfbkvafbhafbafbjafbjafjbafhaahugaugagabghfhaigsgughuaghaghuag",
+  //       date:"11/12/2025"
+  //   }
+  // ];
+
+  const [list,setList]=useState([]);
+
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/task/findAll");
+        setList(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 3;
 
-  const totalPages = Math.ceil(tasks.length / itemsPerPage);
+  const totalPages = Math.ceil(list.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const currentTasks = tasks.slice(startIndex, endIndex);
+  const currentTasks = list.slice(startIndex, endIndex);
+
 
   const handlePrev = () => {
     if (currentPage > 1) setCurrentPage(currentPage - 1);
@@ -85,7 +103,7 @@ function TaskCard() {
   return (
     <>
       {currentTasks.map((task, index) => (
-        <div className="w-full h-40 bg-gray-100 border-2 border-gray-300 rounded-xl mb-2">
+        <div key={task.id} className="w-full h-40 bg-gray-100 border-2 border-gray-300 rounded-xl mb-2">
           <div className="w-full bg-purple-300 h-10  rounded-t-xl flex items-center">
             <input
               type="checkbox"
@@ -94,11 +112,11 @@ function TaskCard() {
             <h1 className="ml-5 text-2xl font-bold">{task.title}</h1>
           </div>
           <div className="h-18 bg-purple-100 ">
-            <p className="ml-10">{task.desp}</p>
+            <p className="ml-10">{task.description}</p>
           </div>
           <div className="w-full h-11 rounded-b-xl flex justify-between items-center bg-purple-200">
             <div className="ml-3 w-30 h-7 rounded-xl flex justify-center items-center border border-purple-500 bg-purple-300">
-              <h3 className="text-purple-600 font-bold">{task.date}</h3>
+              <h3 className="text-purple-600 font-bold">{new Date(task.dueDate).toLocaleDateString("en-GB")}</h3>
             </div>
             <div className="flex gap-3">
               <button>
